9a4a380f35718d5a8890e34d91ad8212ccbc1521
Irafasha Jules <jirafasha@ubuntud.myguest.virtualbox.org>
2024-04-09 20:02:50 +0200

    TASK00

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+shebang to create a PY script
+"""
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAUL_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    """index to run the home route"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
